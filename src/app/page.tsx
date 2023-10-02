24aa33142c3d9f9833d390775b7dfca7cf76c9e8
Shina <53410646+s7tya@users.noreply.github.com>
2023-10-02 20:32:10 +0900

    wip: 見た目を整える

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,80 +87,84 @@ export default function Home() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(search)}>
-        <div>
-          <input placeholder="キーワード" {...register("keyword")} />
-          <button>検索</button>
-          <fieldset>
-            <legend>授業方式</legend>
-
-            <span>
-              <input
-                type="checkbox"
-                id="searchOptions.instructionType.対面"
-                {...register("searchOptions.instructionType.対面")}
-                defaultChecked />
-              <label htmlFor="searchOptions.instructionType.対面">対面</label>
-            </span>
-            <span>
-              <input
-                type="checkbox"
-                id="searchOptions.instructionType.対面(オンライン併用型)"
-                {...register("searchOptions.instructionType.対面(オンライン併用型)")}
-                defaultChecked />
-              <label htmlFor="searchOptions.instructionType.対面(オンライン併用型)">対面(オンライン併用型)</label>
-            </span>
-            <span>
-              <input
-                type="checkbox"
-                id="searchOptions.instructionType.オンデマンド"
-                {...register("searchOptions.instructionType.オンデマンド")}
-                defaultChecked />
-              <label htmlFor="searchOptions.instructionType.オンデマンド">オンデマンド</label>
-            </span>
-          </fieldset>
-          <fieldset>
-            <legend>登録方式</legend>
-
-            <span>
-              <input
-                type="checkbox"
-                id="searchOptions.registrationType.通常科目"
-                {...register("searchOptions.registrationType.通常科目")}
-                defaultChecked />
-              <label htmlFor="searchOptions.registrationType.通常科目">通常科目</label>
-            </span>
-            <span>
-              <input
-                type="checkbox"
-                id="searchOptions.registrationType.事前登録科目"
-                {...register("searchOptions.registrationType.事前登録科目")}
-                defaultChecked />
-              <label htmlFor="searchOptions.registrationType.事前登録科目">事前登録科目</label>
-            </span>
-          </fieldset>
+      <form onSubmit={handleSubmit(search)} className={styles.searchForm}>
+        <div className={styles.searchBox}>
+          <input placeholder="キーワード" {...register("keyword")} className={styles.keywordInput} />
+          <button className={styles.submitButton}>検索</button>
         </div>
+        <fieldset>
+          <legend>授業方式</legend>
+          <span>
+            <input
+              type="checkbox"
+              id="searchOptions.instructionType.対面"
+              {...register("searchOptions.instructionType.対面")}
+              defaultChecked />
+            <label htmlFor="searchOptions.instructionType.対面">対面</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="searchOptions.instructionType.対面(オンライン併用型)"
+              {...register("searchOptions.instructionType.対面(オンライン併用型)")}
+              defaultChecked />
+            <label htmlFor="searchOptions.instructionType.対面(オンライン併用型)">対面(オンライン併用型)</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="searchOptions.instructionType.オンデマンド"
+              {...register("searchOptions.instructionType.オンデマンド")}
+              defaultChecked />
+            <label htmlFor="searchOptions.instructionType.オンデマンド">オンデマンド</label>
+          </span>
+        </fieldset>
+        <fieldset>
+          <legend>登録方式</legend>
+
+          <span>
+            <input
+              type="checkbox"
+              id="searchOptions.registrationType.通常科目"
+              {...register("searchOptions.registrationType.通常科目")}
+              defaultChecked />
+            <label htmlFor="searchOptions.registrationType.通常科目">通常科目</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="searchOptions.registrationType.事前登録科目"
+              {...register("searchOptions.registrationType.事前登録科目")}
+              defaultChecked />
+            <label htmlFor="searchOptions.registrationType.事前登録科目">事前登録科目</label>
+          </span>
+        </fieldset>
 
       </form>
 
-      <p>{result.length}件の検索結果</p>
-
-      <div>
-        {
-          result.map((course) => (
-            <div key={course.科目番号}>
-              <span>{course.科目番号}</span>
-              <h2>{course.科目名}</h2>
-              <div>
-                {course.備考.includes("事前登録対象") && <span>事前登録</span>}
-                {course.備考.includes("対面") && !course.備考.includes("対面(オンライン併用型)") && <span>対面</span>}
-                {course.備考.includes("対面(オンライン併用型)") && <span>対面(オンライン併用型)</span>}
-                {course.備考.includes("オンデマンド") && <span>オンデマンド</span>}
+      <div className={styles.result}>
+        <p className={styles.hitCount}>{result.length}件の検索結果</p>
+        <div className={styles.courses}>
+          {
+            result.map((course) => (
+              <div key={course.科目番号} className={styles.course}>
+                <a href={`https://kdb.tsukuba.ac.jp/syllabi/2023/${course.科目番号}/jpn`} className={styles.link}>
+                  <div>
+                    <span className={styles.code}>{course.科目番号}</span>
+                    <h2 className={styles.title}>{course.科目名}</h2>
+                  </div>
+                  <div className={styles.tags}>
+                    {course.備考.includes("事前登録対象") && <span>事前登録</span>}
+                    {course.備考.includes("対面") && !course.備考.includes("対面(オンライン併用型)") && <span>対面</span>}
+                    {course.備考.includes("対面(オンライン併用型)") && <span>対面(オンライン併用型)</span>}
+                    {course.備考.includes("オンデマンド") && <span>オンデマンド</span>}
+                  </div>
+                  <p>{course.授業概要}</p>
+                </a>
               </div>
-              <a href={`https://kdb.tsukuba.ac.jp/syllabi/2023/${course.科目番号}/jpn`}>シラバス</a>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
 
     </div>
